fb661fd9627ecfd7739b47c2b6ce069b63c20893
clarasmaria10 <olvsclaraa00@gmail.com>
2026-08-21 16:44:40 -0300

    Add files via upload

---
+++ 6 - numeros.py
@@ -0,0 +1,11 @@
+import os
+os.system('cls')
+
+numero = int(input("Digite algum número: "))
+
+sucessor = numero + 1
+antecessor = numero - 1
+
+print('Número: ', numero)
+print("Antecessor: ", antecessor)
+print('Sucessor: ', sucessor)
