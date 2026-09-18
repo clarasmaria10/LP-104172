48822d401e3f44658dce6b17880a4b95f3223905
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-09-18 16:47:42 -0300

    adcionando loops

---
+++ laco_repeticao/atividade1.py
@@ -0,0 +1,7 @@
+import os
+os.system('cls || clear')
+
+for i in range(1, 11):
+    print(f'O número é: {i}')
+
+print('= FIM DO PROGRAMA =')
---
+++ laco_repeticao/tabuada_subtração.py
@@ -0,0 +1,8 @@
+import os
+os.system('cls || clear')
+
+print('= TABUADA =')
+numero = int(input('Digite um número: '))
+
+for i in range(1, 11):
+    print(f'{numero} - {i} = {numero - i}')
